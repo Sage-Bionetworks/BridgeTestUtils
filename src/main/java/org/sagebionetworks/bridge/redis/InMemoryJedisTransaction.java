a8841c820a298517bf3800dfa1e70e0a26f58279
Dwayne Jeng <dwayne.jeng@sagebase.org>
2018-02-16 16:11:29 -0800

    add incr() to InMemoryJedisTransaction

--- src/main/java/org/sagebionetworks/bridge/redis/InMemoryJedisTransaction.java
+++ src/main/java/org/sagebionetworks/bridge/redis/InMemoryJedisTransaction.java
@@ -13,7 +13,7 @@ import redis.clients.jedis.Jedis;
  */
 public class InMemoryJedisTransaction extends JedisTransaction {
     private final InMemoryJedisOps ops;
-    private int numOps = 0;
+    private final List<Object> resultList = new ArrayList<>();
 
     public InMemoryJedisTransaction(InMemoryJedisOps ops) {
         // We need to pass in *something*, otherwise this causes a NullPointerException. However, we don't care what it
@@ -24,31 +24,30 @@ public class InMemoryJedisTransaction extends JedisTransaction {
 
     @Override
     public JedisTransaction setex(String key, int seconds, String value) {
-        ops.setex(key, seconds, value);
-        numOps++;
+        resultList.add(ops.setex(key, seconds, value));
         return this;
     }
 
     @Override
     public JedisTransaction expire(String key, int seconds) {
-        ops.expire(key, seconds);
-        numOps++;
+        resultList.add(ops.expire(key, seconds));
         return this;
     }
 
     @Override public JedisTransaction del(String key) {
-        ops.del(key);
-        numOps++;
+        resultList.add(ops.del(key));
+        return this;
+    }
+
+    @Override
+    public JedisTransaction incr(String key) {
+        resultList.add(ops.incr(key));
         return this;
     }
 
     @Override
     public List<Object> exec() {
         // no-op, since we don't support transactionality
-        List<Object> resultList = new ArrayList<>();
-        for (int i = 0; i < numOps; i++) {
-            resultList.add("OK");
-        }
         return resultList;
     }
 
